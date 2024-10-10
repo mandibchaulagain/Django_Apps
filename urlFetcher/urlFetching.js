38a2f5c3ac592072b712c919b933b0be203f11ef
mandibchaulagain <mandibchaulagain8@gmail.com>
2024-10-10 13:49:30 +0545

    return visible or hidden when tabs are changed

--- urlFetcher/urlFetching.js
+++ urlFetcher/urlFetching.js
@@ -1,3 +1,6 @@
 let fetchvakoname = window.location.href
+let xaVisible = document.visibilityState
 
 document.getElementById('fetchvako').innerHTML = fetchvakoname
+
+document.getElementById('changevoTab').innerHTML = xaVisible
